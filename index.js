06d07f5c51aba8f4ce4bfe3a8736f59f70ecda73
BraxB <braxb92@gmail.com>
2021-04-24 20:18:25 -0400

    created library directory to handle class constructors

--- index.js
+++ index.js
@@ -1,112 +1,12 @@
 // add dependencies
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 const fs = require('fs');
 const inquirer = require('inquirer');
 
 const team = [];
-
-// creates an overarching employee class
-class Employee {
-    constructor(teamMember, id, email){
-        this.teamMember = teamMember;
-        this.id = id;
-        this.email = email;
-    }
-
-    getID() {
-        return this.id;
-    }
-
-    getEmail() {
-        return this.email;
-    }
-
-    getRole() {
-        return 'Employee';
-    }
-}
-
-// creates an Engineer class off of the Employee class w/ function to create card
-class Manager extends Employee {
-    constructor(teamMember, id, email, office) {
-        super(teamMember, id, email);
-        this.office = office;
-    }
-
-    getRole() {
-        return 'Manager';
-    }
-
-    getHTML() {
-        return `
-<div class="card mx-2 my-2" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title">Manager</h5>
-        <p class="card-text">Name: ${this.teamMember}</p>
-        <p class="card-text">ID: ${this.id}</p>
-        <p class="card-text">Office: ${this.office}</p>
-        <a href="mailto:${this.email}" class="btn btn-primary">Email</a>
-    </div>
-</div>`
-    }
-}
-
-// creates an Engineer class off of the Employee class w/ function to create card
-class Engineer extends Employee {
-    constructor(teamMember, id, email, github) {
-        super(teamMember, id, email);
-        this.github = github;
-    }
-
-    getRole() {
-        return 'Engineer'
-    }
-
-    getGithub() {
-        return this.github;
-    }
-
-    getHTML() {
-        return `
-<div class="card mx-2 my-2" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title">Engineer</h5>
-        <p class="card-text">Name: ${this.teamMember}</p>
-        <p class="card-text">ID: ${this.id}</p>
-        <a href="https://github.com/${this.github}" class="btn btn-primary">GitHub</a>
-        <a href="mailto:${this.email}" class="btn btn-primary">Email</a>
-    </div>
-</div>`
-    }
-}
-
-// creates an Intern class off of the Employee class w/ function to create card
-class Intern extends Employee {
-    constructor(teamMember, id, email, school) {
-        super(teamMember, id, email);
-        this.school = school;
-    }
-
-    getSchool() {
-        return this.school;
-    }
-
-    getRole() {
-        return 'Intern'
-    }
-
-    getHTML() {
-        return `
-<div class="card mx-2 my-2" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title">Intern</h5>
-            <p class="card-text">Name: ${this.teamMember}</p>
-            <p class="card-text">ID: ${this.id}</p>
-            <p class="card-text">School: ${this.school}</p>
-            <a href="mailto:${this.email}" class="btn btn-primary">Email</a>
-        </div>
-    </div>`
-    }
-}
 
 // // TODO: Create a function to write HTML file
 function writeHTML(fileName, data) {
